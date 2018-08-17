0c11d6b855f70bd0d24806ce90659dd3342960cb
Smitha6 <alsmith3179@gmail.com>
2018-08-17 17:00:38 -0400

    Initial commit

---
+++ Assets/Code/MapReader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+
+public class MapReader : MonoBehaviour {
+    void Start() {
+        ReadString();
+    }
+    void Update() {
+        
+    }
+    
+    
+    static void ReadString() {
+        int row = 0;
+        int col = 0;
+        char next = '';
+
+        string path = "Assets/Resources/map_1.txt";
+        
+        StreamReader reader = new StreamReader(path);
+
+        string s = reader.ReadToEnd();
+        char[] arr = s.ToCharArray();
+
+        for (int i = 0; i < arr.Length; i++) {
+            next = arr[i];
+
+            if (next == '0') {
+                row++;
+            }
+            else if (next == '1') {
+
+            }
+        }
+
+
+
+
+
+
+        reader.Close();
+
+
+    }
+
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject player;
+
+    private int playerSpeed = 7;
+    private Rigidbody2D body;
+    private Transform my;
+    private Camera cam;
+
+
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+        my = GetComponent<Transform>();
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //---Rotation---
+        //Distance from camera to object.
+        float camDis = cam.transform.position.y - my.position.y;
+        //Get the mouse position in world space. Using camDis for the Z axis.
+        Vector3 mouse = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+        float AngleRad = Mathf.Atan2(mouse.y - my.position.y, mouse.x - my.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+        body.rotation = angle - 90;
+        
+        //---Movement---
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += Vector3.up * Time.deltaTime * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += Vector3.down * Time.deltaTime * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += Vector3.left * Time.deltaTime * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += Vector3.right * Time.deltaTime * playerSpeed;
+        }
+        
+
+    }
+}
